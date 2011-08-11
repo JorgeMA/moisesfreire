39ef0d9da6a2210014a138c2fe504371a166a2a5
Marcos Nogueira <marcosnogueira@live.com>
2011-08-10 22:47:42 -0300

    first interface files

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.9'
-gem 'mysql2', '~> 0.2.6'
+gem 'mysql2', '0.2.6'
+gem 'rake', '0.8.7'
 
 # Deploy with Capistrano
 gem 'capistrano'
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,22 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+$(function(){
+  app.events();
+});
+
+var app = {
+  events: function(){
+    
+  }, //events
+
+  anon: function(){}
+};
+
+
+// HACK para seleção dos browsers.
+  var css_browser_selector = function() {
+  var ua=navigator.userAgent.toLowerCase(),
+  is=function(t){return ua.indexOf(t) != -1;},
+  h=document.getElementsByTagName('html')[0],
+  b=(!(/opera|webtv/i.test(ua))&&/msie (\d)/.test(ua))?('ie ie'+RegExp.$1):is('firefox/2')?'gecko ff2':is('firefox/3')?'gecko ff3':is('gecko/')?'gecko':is('opera/9')?'opera opera9':/opera (\d)/.test(ua)?'opera opera'+RegExp.$1:is('konqueror')?'konqueror':is('applewebkit/')?'webkit safari':is('mozilla/')?'gecko':'',
+  os=(is('x11')||is('linux'))?' linux':is('mac')?' mac':is('win')?' win':'';
+  var c=b+os+' js'; h.className += h.className?' '+c:c;
+  }();
