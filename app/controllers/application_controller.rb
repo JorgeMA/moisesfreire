0c1d9907dbeab548c3cf994185e126d7d569572c
Jorge Moisés Assis <jorgemoisesassis@gmail.com>
2011-08-23 00:25:18 -0300

    begin index integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   
   def home
+    @news = News.where('section_date <= ?', Date.today).order('section_date DESC, id DESC').limit(2)
   end
 end
