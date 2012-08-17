4cba1e4e384f31d26d912b9a87255ca94259d5dc
Jorge Moisés Assis <jorgemoisesassis@gmail.com>
2012-08-16 23:57:45 -0300

    attachment

--- app/models/curriculum.rb
+++ app/models/curriculum.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Curriculum < ActiveRecord::Base
   has_attached_file :attachment
-  validates_attachment :attachment, :presence => true, :content_type => { :content_type => "document/pdf" }, :size => { :in => 0..1000.kilobytes }
+  validates :attachment, :attachment_presence => true, :attachment_content_type => "document/pdf", :size => { :in => 0..1000.kilobytes }
   validates_presence_of :name
   validates_presence_of :brief_description
 end
--- app/models/news.rb
+++ app/models/news.rb
@@ -3,5 +3,5 @@ class News < ActiveRecord::Base
   validate :title, :presence => true, :length => {:within => 1..255}
   validates_presence_of :content
   has_attached_file :picture, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  validates_attachment :picture, :content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/bmp"]}
+  validates :picture, :attachment_content_type => ["image/jpg", "image/jpeg", "image/png", "image/bmp"]
 end
